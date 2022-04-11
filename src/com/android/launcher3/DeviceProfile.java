5a376609be821e0f4098372c4c31a30b7a14244c
Danny Lin <danny@kdrag0n.dev>
2022-04-10 18:38:45 -0700

    Launcher3: Add support for toggling taskbar
    - Use new API to enable/disable SysUI taskbar integration
    - User-facing setting to control taskbar eligibility
    - Reload by restarting (live reload would be much more complicated)
    - Update SysUI integration state before reloading
    
    Depends on frameworks/base commit:
        SystemUI: Add API for runtime taskbar config
    
    Change-Id: Id1889ca9b102f94cc0a1161f49de88428dcc0943
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: anoosragh69 <ok.ano0s6090@gmail.com>

--- src/com/android/launcher3/DeviceProfile.java
+++ src/com/android/launcher3/DeviceProfile.java
@@ -70,6 +70,8 @@ import com.android.launcher3.util.ResourceHelper;
 import com.android.launcher3.util.WindowBounds;
 import com.android.launcher3.util.window.WindowManagerProxy;
 
+import lineageos.providers.LineageSettings;
+
 import java.io.PrintWriter;
 import java.util.Locale;
 import java.util.function.Consumer;
@@ -416,8 +418,9 @@ public class DeviceProfile {
         isTablet = info.isTablet(windowBounds);
         isPhone = !isTablet;
         isTwoPanels = isTablet && isMultiDisplay;
-        isTaskbarPresent = (isTablet || (enableTinyTaskbar() && isGestureMode))
-                && WindowManagerProxy.INSTANCE.get(context).isTaskbarDrawnInProcess();
+        boolean isTaskBarEnabled = LineageSettings.System.getInt(context.getContentResolver(),
+                LineageSettings.System.ENABLE_TASKBAR, isTablet ? 1 : 0) == 1;
+        isTaskbarPresent = isTaskBarEnabled && ApiWrapper.TASKBAR_DRAWN_IN_PROCESS;
 
         // Some more constants.
         context = getContext(context, info, isVerticalBarLayout() || (isTablet && isLandscape)
