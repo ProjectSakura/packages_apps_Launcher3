4ada27dbd0bc331c301fe2279631fc258280f0e4
LibXZR <i@xzr.moe>
2022-04-30 09:50:39 +0800

    Launcher3: Do not attach cleanupScreenshot() to frame update event
    When the animation get canceled, cleanupScreenshot() should
    be run instantly because it does something like removing
    pending animations. The switchToScreenshot() attaches it to
    RtFrameCallback which only be called on frame update. This
    is leading to problems as the next frame update may only
    happened when the next gesture starts, which means all the
    pending animations will be removed on next gesture. And then
    the next gesture freezed.
    
    Fixes randomly gesture freeze since 12L.
    
    Change-Id: I10247294a2dcae467706c434685b299f8b525888
    Signed-off-by: LibXZR <i@xzr.moe>
    Signed-off-by: anoosragh69 <ok.ano0s6090@gmail.com>

--- quickstep/src/com/android/quickstep/AbsSwipeUpHandler.java
+++ quickstep/src/com/android/quickstep/AbsSwipeUpHandler.java
@@ -535,8 +535,13 @@ public abstract class AbsSwipeUpHandler<
             HashMap<Integer, ThumbnailData> snapshots =
                     mGestureState.consumeRecentsAnimationCanceledSnapshot();
             if (snapshots != null) {
-                mRecentsView.switchToScreenshot(snapshots, () -> {});
                 mRecentsView.onRecentsAnimationComplete();
+                if (mRecentsAnimationController != null) {
+                    mRecentsAnimationController.cleanupScreenshot();
+                } else if (mDeferredCleanupRecentsAnimationController != null) {
+                    mDeferredCleanupRecentsAnimationController.cleanupScreenshot();
+                    mDeferredCleanupRecentsAnimationController = null;
+                }
             }
         });
 
