8ce3373ebaad9a0c58b6c19494fed445b075d449
Tommy Webb <tommy@calyxinstitute.org>
2022-12-14 16:13:06 -0500

    Launcher3: Use different badges for different work profiles
    * Supply the UserHandle to BitmapIcon, rather than FLAG_WORK, so that
      the proper badge icon can be used for a given profile (different
      colors).
    
    Change-Id: I64f1b425eee5cc7994616050411a881d84ece99d
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: anoosragh69 <ok.ano0s6090@gmail.com>

--- src/com/android/launcher3/widget/WidgetCell.java
+++ src/com/android/launcher3/widget/WidgetCell.java
@@ -56,6 +56,7 @@ import com.android.launcher3.Launcher;
 import com.android.launcher3.LauncherAppState;
 import com.android.launcher3.R;
 import com.android.launcher3.anim.AnimatedPropertySetter;
+import com.android.launcher3.icons.BaseIconFactory;
 import com.android.launcher3.icons.FastBitmapDrawable;
 import com.android.launcher3.icons.RoundDrawableWrapper;
 import com.android.launcher3.model.WidgetItem;
@@ -419,6 +420,26 @@ public class WidgetCell extends LinearLayout {
         if (mActiveRequest != null) {
             mActiveRequest.cancel();
             mActiveRequest = null;
+        }
+    }
+
+    /** Used to show the badge when the widget is in the recommended section
+     */
+    public void showBadge() {
+        if (Process.myUserHandle().equals(mItem.user)) {
+            mWidgetBadge.setVisibility(View.GONE);
+        } else {
+            final int iconSize = mActivity.getDeviceProfile().allAppsIconSizePx;
+            try (BaseIconFactory bif = LauncherAppState.getInstance(getContext()).getIconCache()
+                    .getIconFactory()) {
+                final Drawable badge = bif.getBadgeForUser(mItem.user, iconSize);
+                if (badge != null) {
+                    mWidgetBadge.setVisibility(View.VISIBLE);
+                    mWidgetBadge.setImageDrawable(badge);
+                } else {
+                    mWidgetBadge.setVisibility(View.GONE);
+                }
+            }
         }
     }
 
