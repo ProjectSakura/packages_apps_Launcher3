d55099b519201316d3a0ded8d51b272521b943e1
Pranav Vashi <neobuddy89@gmail.com>
2023-04-07 08:40:20 +0530

    Launcher3: Add separate toggle for shake to clear tasks
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: anoosragh69 <ok.ano0s6090@gmail.com>

--- quickstep/src/com/android/quickstep/views/OverviewActionsView.java
+++ quickstep/src/com/android/quickstep/views/OverviewActionsView.java
@@ -119,6 +119,7 @@ public class OverviewActionsView<T extends OverlayUICallbacks> extends FrameLayo
     private static final String KEY_RECENTS_SCREENSHOT = "pref_recents_screenshot";
     private static final String KEY_RECENTS_CLEAR_ALL = "pref_recents_clear_all";
     private static final String KEY_RECENTS_LENS = "pref_recents_lens";
+    private static final String KEY_RECENTS_SHAKE_CLEAR_ALL = "pref_recents_shake_clear_all";
     private Button mSplitButton;
     /**
      * The "save app pair" button. Currently this is the only button that is not contained in
@@ -148,6 +149,7 @@ public class OverviewActionsView<T extends OverlayUICallbacks> extends FrameLayo
     private boolean mScreenshot;
     private boolean mClearAll;
     private boolean mLens;
+    private boolean mShakeClearAll;
 
     public OverviewActionsView(Context context) {
         this(context, null);
@@ -164,23 +166,17 @@ public class OverviewActionsView<T extends OverlayUICallbacks> extends FrameLayo
         mClearAll = prefs.getBoolean(KEY_RECENTS_CLEAR_ALL, true);
         mLens = prefs.getBoolean(KEY_RECENTS_LENS, false);
         prefs.registerOnSharedPreferenceChangeListener(this);
-    }
-
-    private void bindShake() {
-        mShakeUtils.bindShakeListener(this);
-    }
-
-    private void unBindShake() {
-        mShakeUtils.unBindShakeListener(this);
+        mShakeUtils = new ShakeUtils(context);
+        mShakeClearAll = prefs.getBoolean(KEY_RECENTS_SHAKE_CLEAR_ALL, true);
     }
 
     @Override
     public void onVisibilityAggregated(boolean isVisible) {
         super.onVisibilityAggregated(isVisible);
         if (isVisible) {
-            bindShake();
+            mShakeUtils.bindShakeListener(this);
         } else {
-            unBindShake();
+            mShakeUtils.unBindShakeListener(this);
         }
     }
 
@@ -207,7 +203,6 @@ public class OverviewActionsView<T extends OverlayUICallbacks> extends FrameLayo
                 }
             }, 1f /* initialValue */);
         }
-        mShakeUtils = new ShakeUtils(getContext());
         updateVisibilities();
     }
 
@@ -234,9 +229,8 @@ public class OverviewActionsView<T extends OverlayUICallbacks> extends FrameLayo
 
     @Override
     public void onShake(double speed) {
-        if (mCallbacks != null && findViewById(R.id.action_clear_all).getVisibility() == VISIBLE) {
+        if (mCallbacks != null && mShakeClearAll) {
             mCallbacks.onClearAllTasksRequested();
-            setCallbacks(null); // Clear the listener after shake
         }
     }
 
@@ -289,6 +283,8 @@ public class OverviewActionsView<T extends OverlayUICallbacks> extends FrameLayo
             mClearAll = prefs.getBoolean(KEY_RECENTS_CLEAR_ALL, true);
         } else if (key.equals(KEY_RECENTS_LENS)) {
             mLens = prefs.getBoolean(KEY_RECENTS_LENS, false);
+        } else if (key.equals(KEY_RECENTS_SHAKE_CLEAR_ALL)) {
+            mShakeClearAll = prefs.getBoolean(KEY_RECENTS_SHAKE_CLEAR_ALL, true);
         }
         updateVisibilities();
     }
